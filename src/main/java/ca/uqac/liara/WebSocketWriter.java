595704f93ea73c625c218179946bcc1b43f13b59
Baptiste Lemarcis <Baptiste.Lemarcis@gmail.com>
2016-10-06 14:59:28 -0400

    Initial commit, setup repo

--- src/main/java/ca/uqac/liara/WebSocketWriter.java
+++ src/main/java/ca/uqac/liara/WebSocketWriter.java
@@ -3,15 +3,18 @@ package ca.uqac.liara;
 import ca.uqac.lif.cep.Processor;
 import ca.uqac.lif.cep.SingleProcessor;
 
+
 import java.util.Queue;
 
 /**
  * Created by Baptiste on 10/6/2016.
  */
 public class WebSocketWriter extends SingleProcessor {
+    private String uri;
 
-    public WebSocketWriter(int in_arity, int out_arity) {
-        super(in_arity, out_arity);
+    public WebSocketWriter(int in_arity, String uri) {
+        super(in_arity, 0);
+        this.uri = uri;
     }
 
     @Override
