59fb9148fdb12395deee69466663d42c77f5a7fc
Baptiste Lemarcis <Baptiste.Lemarcis@gmail.com>
2016-10-06 16:11:13 -0400

    Add missing library

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import ca.uqac.liara.sources.RandomStreamer;
+
 /**
  * Created by Baptiste on 10/6/2016.
  */
 public class Main {
     public static void main(String[] argv){
+        RandomStreamer rs = new RandomStreamer();
 
     }
 }
